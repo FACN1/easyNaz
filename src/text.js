6af3b9c7839071b2c0157006d36e45493b748bb8
Matt Davis <matthewpeterdavis@gmail.com>
2017-05-16 20:55:36 +0300

    make language object

---
+++ src/text.js
@@ -0,0 +1,34 @@
+module.exports = {
+  // General
+  mainTitle: 'easy Naz',
+  help: 'Help',
+  submit: 'Submit',
+  // Accessibility options
+  access: 'Accessibility Options:',
+  braille: 'Braille',
+  largeFont: 'Large Font Menus',
+  wheelchair: 'WheelChair Access',
+  audioMenu: 'Audio Menu',
+  // Biz Info Page
+  address: 'Address',
+  desc: 'Description: ',
+  opens: 'Opening Hours: ',
+  contact: 'Contact details',
+  phone: 'Phone Number: ',
+  email: 'Email: ',
+  // Biz Options
+  bizChoice: 'What kind of business are you looking for?',
+  rest: 'Restaurants',
+  religion: 'Religion',
+  shopping: 'Shopping',
+  museums: 'Museums',
+  // Loc Options
+  locChoice: 'Please enter the location you are looking for: ',
+  loc1: "Mary's Well",
+  loc2: 'Area 2',
+  loc3: 'Area 3',
+  loc4: 'Area 4',
+  loc5: 'Area 5',
+  loc6: 'Area 6',
+  loc7: 'Area 7'
+}
