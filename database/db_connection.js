48f5bae84b196d05a1caea88e7452b5ea2188b21
hiba akel <hibaakel226@gmail.com>
2017-05-23 18:14:51 +0300

    make schema for mongoose and functions to build fake database

--- database/db_connection.js
+++ database/db_connection.js
@@ -2,19 +2,49 @@ require('env2')('./config.env')
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI)
 
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', () => {
-  // we're connected!
-  const kittySchema = mongoose.Schema({
-    name: String
-  })
-  const Kitten = mongoose.model('Kitten', kittySchema)
-  const keety = new Kitten({name: 'suha'})
-  keety.save((error, result) => {
-    if (error) {
-      console.log(error)
-    }
-    db.close()
-  })
+const fs = require('fs')
+const mock = JSON.parse(fs.readFileSync('mock data/mock-businesses.json'))
+
+const businessSchema = mongoose.Schema({
+  name: String,
+  loc: String,
+  acessOptions: [],
+  open: String,
+  desc: String,
+  fb: String,
+  website: String,
+  phoneNumber: String,
+  email: String
 })
+
+const Business = mongoose.model('business', businessSchema)
+
+const buildFake = () => {
+  const db = mongoose.connection
+  db.on('error', console.error.bind(console, 'connection error:'))
+  db.once('open', () => {
+    // we're connected!
+    mock.forEach((fakeData) => {
+      const business = new Business(fakeData)
+
+      business.save((err, res) => {
+        if (err) console.log(err)
+        console.log('business added')
+      })
+    })
+  })
+}
+
+const showDb = () => {
+  const db = mongoose.connection
+  db.on('error', console.error.bind(console, 'connection error:'))
+  db.once('open', () => {
+    // we're connected!
+    Business.find((err, businessess) => {
+      if (err) console.log(err)
+      console.log(businessess)
+    })
+  })
+}
+
+showDb()
