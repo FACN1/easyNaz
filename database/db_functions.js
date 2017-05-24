1202a1b24021754d073dc14b3a49aa9a19231591
Matt Davis <matthewpeterdavis@gmail.com>
2017-05-24 15:20:58 +0300

    fix typo

--- database/db_functions.js
+++ database/db_functions.js
@@ -6,7 +6,7 @@ mongoose.connect(process.env.MONGODB_URI)
 const businessSchema = mongoose.Schema({
   name: String,
   loc: String,
-  Catagory: [String],
+  category: [String],
   accessOptions: [String],
   open: String,
   desc: String,
