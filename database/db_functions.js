4bdc4e751192510f2aa06e43378462e2f6f8a9d5
Matt Davis <matthewpeterdavis@gmail.com>
2017-05-24 15:16:11 +0300

    add catagory option into mock data

--- database/db_functions.js
+++ database/db_functions.js
@@ -6,6 +6,7 @@ mongoose.connect(process.env.MONGODB_URI)
 const businessSchema = mongoose.Schema({
   name: String,
   loc: String,
+  Catagory: [String],
   accessOptions: [String],
   open: String,
   desc: String,
