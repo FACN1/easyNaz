98eb55c55f7abf9c247ef46c0d9d88897f2e4ef3
suha-edris <suha.edris@gmail.com>
2017-05-28 13:02:10 +0300

    add routes to services page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 
 const homePage = require('./home.js')
+const servicesPage = require('./services.js')
 
 router.get('/', homePage)
+router.get('/services', servicesPage)
 
 module.exports = router
