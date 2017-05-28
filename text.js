7ba8dc146d2823b392ceb6af197cc08a795adfb3
suha-edris <suha.edris@gmail.com>
2017-05-28 13:00:01 +0300

    add arabic and english services keys

--- text.js
+++ text.js
@@ -2,19 +2,39 @@ module.exports = {
   english: { // General
     mainTitle: 'easy Naz',
     help: 'Help',
-    submit: 'Next',
+    submit: 'get resutls',
+    next: 'go to the next page',
     back: 'go back to the previous page',
     homePage: 'Go-back-to-the-Start',
+    serviceTitle: 'Services Page',
     homeIcon: 'Icon for the home page',
-    helpIcon: 'Icon for the help button'
+    helpIcon: 'Icon for the help button',
+    serviceQuesion: 'What kind of service are you looking for',
+    food: 'Food',
+    shopping: 'Shopping',
+    religion: 'Religion',
+    health: 'Health',
+    viewPoints: 'View Points',
+    nature: 'Nature',
+    museums: 'Museums'
   },
   arabic: { // General
     mainTitle: 'easy Naz',
     help: ' مساعده',
-    submit: 'التالي ',
+    submit: 'اعرض النتائج ',
+    next: 'التالي ',
     back: 'العوده صفحه الى الوراء ',
     homePage: 'العوده الى الصفحه الرئيسيه ',
+    serviceTitle: 'صفحة الخدمات ',
     homeIcon: ' ايقونه الصفحه الرئيسيه ',
-    helpIcon: ' ايقونه للمساعده'
+    helpIcon: ' ايقونه للمساعده',
+    serviceQuesion: 'ما نوع الخدمة او الخدمات التي تبحث عنها ',
+    food: 'طعام',
+    shopping: 'مشتريات',
+    religion: 'ديانات',
+    health: 'صحة',
+    viewPoints: 'مناظر',
+    nature: 'طبيعة',
+    museums: 'متاحف'
   }
 }
