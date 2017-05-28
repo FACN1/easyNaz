2844d30923ea7d0e7065128021fce277c0be2f34
Matt Davis <matthewpeterdavis@gmail.com>
2017-05-28 14:35:59 +0300

    change text to reference the text object #99

--- text.js
+++ text.js
@@ -3,10 +3,18 @@ module.exports = {
     mainTitle: 'easy Naz',
     help: 'Help',
     submit: 'Next',
+    nextAria: 'Go to the next page',
     back: 'go back to the previous page',
     homePage: 'Go-back-to-the-Start',
     homeIcon: 'Icon for the home page',
-    helpIcon: 'Icon for the help button'
+    helpIcon: 'Icon for the help button',
+    filtersChosen: 'Filters Chosen',
+    // Disabilities page
+    disabilitiesPrompt: 'Select the Disability or Disabilities you want to be catered for',
+    visual: 'Visual',
+    physical: 'Physical',
+    aural: 'Aural',
+    mental: 'Mental'
   },
   arabic: { // General
     mainTitle: 'easy Naz',
@@ -15,6 +23,13 @@ module.exports = {
     back: 'العوده صفحه الى الوراء ',
     homePage: 'العوده الى الصفحه الرئيسيه ',
     homeIcon: ' ايقونه الصفحه الرئيسيه ',
-    helpIcon: ' ايقونه للمساعده'
+    helpIcon: ' ايقونه للمساعده',
+    filtersChosen: 'Filters Chosen (in Arabic)',
+    // Disabilities page
+    disabilitiesPrompt: 'Select the Disability or Disabilities you want to be catered for (in Arabic)',
+    visual: 'Visual (in arabic)',
+    physical: 'Physical (in arabic)',
+    aural: 'Aural (in arabic)',
+    mental: 'Mental (in arabic)'
   }
 }
