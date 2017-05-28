b95500f4bf196f37a497d5fae9d13e9dfd691519
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-05-28 12:10:08 +0300

    add helpInfo to the objects

--- text.js
+++ text.js
@@ -6,7 +6,8 @@ module.exports = {
     back: 'go back to the previous page',
     homePage: 'Go-back-to-the-Start',
     homeIcon: 'Icon for the home page',
-    helpIcon: 'Icon for the help button'
+    helpIcon: 'Icon for the help button',
+    helpInfo: 'easyNaz is made in aim to let people know of the few accissible places in Nazareth and encourage more businesses to become accessible! In our app you can find all the accessible places and businesses in Nazrareth anytime and just a few clicks depends on what you need or looking for.'
   },
   arabic: { // General
     mainTitle: 'easy Naz',
@@ -15,6 +16,7 @@ module.exports = {
     back: 'العوده صفحه الى الوراء ',
     homePage: 'العوده الى الصفحه الرئيسيه ',
     homeIcon: ' ايقونه الصفحه الرئيسيه ',
-    helpIcon: ' ايقونه للمساعده'
+    helpIcon: ' ايقونه للمساعده',
+    helpInfo: 'يرصد إيسيناز في تهدف إلى السماح للناس يعرفون من عدد قليل من الأماكن التي يمكن الوصول إليها في الناصرة وتشجيع المزيد من الشركات لتصبح في متناول!    في التطبيق لدينا يمكنك أن تجد جميع الأماكن التي يمكن الوصول إليها والشركات في نازارث في أي وقت وعدد قليل من النقرات يعتمد على ما تحتاجه أو تبحث عن'
   }
 }
