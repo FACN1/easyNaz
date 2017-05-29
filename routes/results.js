835a8135e8d4e4bb890016f0c06569729cfe051f
hiba akel <hibaakel226@gmail.com>
2017-05-29 16:03:41 +0300

    take the url and convert to string

--- routes/results.js
+++ routes/results.js
@@ -1,8 +1,15 @@
 const fs = require('fs')
-
 const mockResult = JSON.parse(fs.readFileSync('./mock data/mock_result.json'))
 
 module.exports = (req, res) => {
+  const disabilityOption = req.query.disability.toString()
+  // const serviceOption = req.query.service.toString()
+
+  const options = ['visual', 'aural', 'mental', 'Physical']
+
+  if (disabilityOption === options) {
+    console.log('wheelchair')
+  }
   res.render('result', {
     results: mockResult
   })
