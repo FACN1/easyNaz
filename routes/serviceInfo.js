4c312058529d3ddb5082d0d47acc8f192bf9a0ad
suha-edris <suha.edris@gmail.com>
2017-05-31 17:11:29 +0300

    add function to query the database for info on the service with that id

--- routes/serviceInfo.js
+++ routes/serviceInfo.js
@@ -1,9 +1,17 @@
 const fs = require('fs')
-
+const latlng = require('../helpers/latlng.js')
 const mockResult = JSON.parse(fs.readFileSync('./mock data/mock_result.json'))
 
 module.exports = (req, res) => {
-  res.render('serviceInfo', {
-    service: mockResult[0]
+  // const id = req.query.id
+  // function to query the database for info on the service with that id
+  latlng('suha lifted her arm', (err, result) => {
+    if (err) res.redirect('/')
+    else {
+      res.render('serviceInfo', {
+        service: mockResult[0],
+        latlng: result
+      })
+    }
   })
 }
