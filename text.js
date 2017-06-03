469728fc76b61664f3d7e209a4722cf8b93aa3d9
Matt Davis <matthewpeterdavis@gmail.com>
2017-06-03 12:52:58 +0300

    add remove button text to the text.js file and pass it using the dataset attribute in handlebars

--- text.js
+++ text.js
@@ -12,6 +12,7 @@ module.exports = {
     homeIcon: 'Icon for the home page',
     helpIcon: 'Icon for the help button',
     helpInfo: 'easyNaz is made in aim to let people know of the few accissible places in Nazareth and encourage more businesses to become accessible! In our app you can find all the accessible places and businesses in Nazrareth anytime and just a few clicks depends on what you need or looking for.',
+    removeFilter: 'Remove-Filter',
     // Disabilities page
     disabilitiesPrompt: 'Select the Disability or Disabilities you want to be catered for',
     visual: 'Visual',
@@ -41,6 +42,7 @@ module.exports = {
     homeIcon: ' ايقونه الصفحه الرئيسيه ',
     helpIcon: ' ايقونه للمساعده',
     helpInfo: ' هذا التطبيق بني بهدف مساعدة الناس على التعرف على بعض الاماكن في الناصرة التي توفر خدمات لذوي الاحتياجات الخاصة ولتشجيع اصحاب المصالح التجارية لادخال هذة الخدمات ضمن عملهم ومن خلال تطبيقنا تستطيع ان تجد اي خدمة تبحث عنها في اي وقت هي مجرد كبسة زر حتى تجد ما تبحث عنه',
+    removeFilter: 'Remove Filter In arabic',
     // Disabilities page
     disabilitiesPrompt: 'حدد نوع الاعاقة او الاعاقات التي تهمك ',
     visual: 'بصري',
