a54d6b5476dfbabc8a040d8a9035fc040259d1bf
Matt Davis <matthewpeterdavis@gmail.com>
2017-06-04 14:39:33 +0300

    get lat long of the address to pass to the map

--- routes/serviceInfo.js
+++ routes/serviceInfo.js
@@ -1,14 +1,20 @@
-// const latlng = require('../helpers/latlng.js')
+const latLng = require('../helpers/latlng.js')
 const dbFunctions = require('../database/db_functions.js')
 
 module.exports = (req, res) => {
   const id = req.query.id
   dbFunctions.findById(dbFunctions.Business, id, (error, service) => {
-    if (error) {
-      res.render('notFound')
-    } else {
-      res.render('serviceInfo', {
-        service: service[0]
+    if (error) res.render('notFound')
+    else {
+      // coordinates means إحداثيات
+      latLng(service[0].Loc, (err, coordinates) => {
+        if (err) res.render('notFound')
+        else {
+          res.render('serviceInfo', {
+            service: service[0],
+            latlng: coordinates
+          })
+        }
       })
     }
   })
