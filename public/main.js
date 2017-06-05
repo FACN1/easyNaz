fdd5bb05f24c67ac70fc526c24826f065698ca18
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-06-05 16:19:51 +0300

    create addeventListener -change- function

--- public/main.js
+++ public/main.js
@@ -3,3 +3,9 @@ help.addEventListener('click', function (event) {
   var showInformation = document.getElementById('show-information')
   showInformation.classList.toggle('dn')
 })
+
+var languageBtn = document.querySelector('#languageBtn')
+
+languageBtn.addEventListener('change', function (event) {
+  this.submit()
+})
